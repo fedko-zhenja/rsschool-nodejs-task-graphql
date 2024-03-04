9e68ef5d0c74dc4de43ec7de7357e30c54a3b349
@fedko-zhenja <fedkozhenja@gmail.com>
2024-03-04 20:47:31 +0400

    feat: implement four tests

--- src/routes/graphql/types/profileType.ts
+++ src/routes/graphql/types/profileType.ts
@@ -1,9 +1,12 @@
 import { GraphQLObjectType, GraphQLNonNull, GraphQLInputObjectType, GraphQLBoolean, GraphQLInt  } from 'graphql';
 import { UUIDType } from './uuid.js';
 import { MemberEnumTypeForId } from './memberType.js';
+import { MemberType } from './memberType.js';
+import { Profile } from "@prisma/client";
+import { Context } from './interface.js';
 
-export const ProfileType = new GraphQLObjectType({
-    name: 'ProfileType',
+export const ProfileType = new GraphQLObjectType<Profile, Context>({
+    name: 'Profile',
     fields: () => ({
         id: {
             type: new GraphQLNonNull(UUIDType)
@@ -20,38 +23,44 @@ export const ProfileType = new GraphQLObjectType({
         memberTypeId: {
             type: new GraphQLNonNull(MemberEnumTypeForId)
         },
+        memberType: {
+            type: new GraphQLNonNull(MemberType),
+            resolve (parent, args, ctx) {
+                return ctx.prisma.memberType.findUnique({ where: { id: parent.memberTypeId } });
+              },
+        }
     })
 });
 
-export const ProfileTypePATCH = new GraphQLInputObjectType({
-    name: 'ProfileTypePATCH',
-    fields: () => ({
-        isMale: {
-            type: new GraphQLNonNull(GraphQLBoolean)
-        },
-        yearOfBirth: {
-            type: new GraphQLNonNull(GraphQLInt)
-        },
-        memberTypeId: {
-            type: new GraphQLNonNull(MemberEnumTypeForId)
-        },
-    })
-})
+// export const ProfileTypePATCH = new GraphQLInputObjectType({
+//     name: 'ProfileTypePATCH',
+//     fields: () => ({
+//         isMale: {
+//             type: new GraphQLNonNull(GraphQLBoolean)
+//         },
+//         yearOfBirth: {
+//             type: new GraphQLNonNull(GraphQLInt)
+//         },
+//         memberTypeId: {
+//             type: new GraphQLNonNull(MemberEnumTypeForId)
+//         },
+//     })
+// })
 
-export const ProfileTypePOST = new GraphQLInputObjectType({
-    name: 'ProfileTypePOST',
-    fields: () => ({
-        isMale: {
-            type: new GraphQLNonNull(GraphQLBoolean)
-        },
-        yearOfBirth: {
-            type: new GraphQLNonNull(GraphQLInt)
-        },
-        userId: {
-            type: new GraphQLNonNull(UUIDType)
-        },
-        memberTypeId: {
-            type: new GraphQLNonNull(MemberEnumTypeForId)
-        },
-    })
-})
+// export const ProfileTypePOST = new GraphQLInputObjectType({
+//     name: 'ProfileTypePOST',
+//     fields: () => ({
+//         isMale: {
+//             type: new GraphQLNonNull(GraphQLBoolean)
+//         },
+//         yearOfBirth: {
+//             type: new GraphQLNonNull(GraphQLInt)
+//         },
+//         userId: {
+//             type: new GraphQLNonNull(UUIDType)
+//         },
+//         memberTypeId: {
+//             type: new GraphQLNonNull(MemberEnumTypeForId)
+//         },
+//     })
+// })
--- src/routes/graphql/types/userType.ts
+++ src/routes/graphql/types/userType.ts
@@ -3,9 +3,11 @@ import { UUIDType } from './uuid.js';
 // import { MemberEnumTypeForId } from './memberType.js';
 import { ProfileType } from './profileType.js';
 import { PostType } from './postType.js';
+import { User } from "@prisma/client";
+import { Context } from './interface.js';
 
-export const UserType: GraphQLObjectType = new GraphQLObjectType({
-    name: 'UserType',
+export const UserType: GraphQLObjectType = new GraphQLObjectType<User, Context>({
+    name: 'User',
     fields: () => ({
         id: {
             type: new GraphQLNonNull(UUIDType)
@@ -19,54 +21,81 @@ export const UserType: GraphQLObjectType = new GraphQLObjectType({
 
         profile: {
             type: ProfileType,
+            resolve (parent, args, ctx) {
+                return ctx.prisma.profile.findUnique({ where: { userId: parent.id } });
+            },
         },
 
         posts: {
-            type: PostType,
+            type: new GraphQLList(PostType) ,
+            resolve (parent, args, ctx) {
+                return ctx.prisma.post.findMany({ where: { authorId: parent.id } });
+            },
         },
 
+        // userSubscribedTo: {
+        //     type: new GraphQLList(UserType),
+        //     resolve: async (parent, args, ctx) => {
+        //         const res = (
+        //             (await ctx.prisma.post.findMany({ where: { subscriberId: parent.id }, select: { author: true } })).map(({author}) => author);
+        //             return res
+        //         )
+                
+        //     },
+        // },
+
         userSubscribedTo: {
-            type: new GraphQLList(SubscribersOnAuthorsType),
+            type: new GraphQLList(UserType),
+            resolve: async (parent, args, ctx) => {
+                const res = (await ctx.prisma.subscribersOnAuthors.findMany({ where: { subscriberId: parent.id }, select: { author: true } })).map(({ author }) => author);
+
+                return res;
+            } 
         },
 
         subscribedToUser: {
-            type: new GraphQLList(SubscribersOnAuthorsType),
+            type: new GraphQLList(UserType),
+            resolve: async (parent, args, ctx) => {
+                const res = (await ctx.prisma.subscribersOnAuthors.findMany({ where: { authorId: parent.id }, select: { subscriber: true } })).map(({ subscriber }) => subscriber);
+
+                return res;
+            } 
         }
     })
 });
 
-export const SubscribersOnAuthorsType = new GraphQLObjectType({
-    name: 'SubscribersOnAuthorsType',
-    fields: () => ({
-        subscriber: { 
-            type:  UserType
-        },
-        author: { 
-            type: UserType
-        }
-    })
-});
+// export const SubscribersOnAuthorsType = new GraphQLObjectType({
+//     name: 'SubscribersOnAuthorsType',
+//     fields: () => ({
+//         subscriber: { 
+//             type:  UserType
+//         },
+//         author: { 
+//             type: UserType
+//         }
+//     })
+// });
 
-export const UserTypePATCH = new GraphQLInputObjectType({
-    name: 'UserTypePATCH',
-    fields: () => ({
-        name: {
-            type: new GraphQLNonNull(GraphQLString)
-        },
-        balance: {
-            type: new GraphQLNonNull(GraphQLFloat)
-        }
-    })
-})
+// export const UserTypePATCH = new GraphQLInputObjectType({
+//     name: 'UserTypePATCH',
+//     fields: () => ({
+//         name: {
+//             type: new GraphQLNonNull(GraphQLString)
+//         },
+//         balance: {
+//             type: new GraphQLNonNull(GraphQLFloat)
+//         }
+//     })
+// })
 
-export const UserTypePOST = new GraphQLInputObjectType({
-    name: 'UserTypePOST',
-    fields: () => ({
-        name: {
-            type: new GraphQLNonNull(GraphQLString)
-        },
-        balance: {
-            type: new GraphQLNonNull(GraphQLFloat)
-        }
-    })
-})
+// export const UserTypePOST = new GraphQLInputObjectType({
+//     name: 'UserTypePOST',
+//     fields: () => ({
+//         name: {
+//             type: new GraphQLNonNull(GraphQLString)
+//         },
+//         balance: {
+//             type: new GraphQLNonNull(GraphQLFloat)
+//         }
+//     })
+// })
